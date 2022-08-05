ccb921d89bd9ed39a8c5ebef42aca1a91c881d91
Arkamitra Mukherjee <mukherjeearkamitra@gmail.com>
2022-08-06 00:26:43 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+const app = express();
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+app.get("/",function(req,res){
+    res.sendFile(__dirname+ "/signUp.html");
+})
+app.post("/",function(req,res){
+    const firstName = req.body.fName;
+    const lastName = req.body.lName;
+    const email = req.body.email;
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME : firstName,
+                    LNAME : lastName
+                }
+            }
+        ]
+    };
+    const jsonData = JSON.stringify(data);
+    const url = "https://us14.api.mailchimp.com/3.0/lists/1bf071062b";
+    const options={
+        method : "POST",
+        auth : "arkamitra:b29ec8de3cb392c522454d489f7d2ea3-us14"
+    }
+    const request1=https.request(url,options,function(response){
+        if(response.statusCode===200){
+            res.sendFile(__dirname+"/success.html");
+        }
+        else{
+           res.sendFile(__dirname+"/failure.html");
+        }
+        response.on("data",function(data){
+            console.log(JSON.parse(data));
+        })
+    });
+    request1.write(jsonData);
+    request1.end();
+});
+app.post("/failure",function(req,res){
+    res.redirect("/");
+})
+app.listen(process.env.PORT || 3000,function(){
+    console.log("server is running on port 3000");
+})
+//b29ec8de3cb392c522454d489f7d2ea3-us14 ->api key
+//1bf071062b ->unique id
